89d80fb90f1ba469c5163f17897ebd34d81319d5
fAEDKAN <sfdk.94@hotmail.com>
2022-08-11 19:47:47 -0300

    form created

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(express.static('public'));
 //* RUTAS
 
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')));
-
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')));
 
 //* CONSOLE LOG
 
